89ad9ee041de7eafe1eeb7f18a9bd4f7ae4e98ad
Ziqiao John Lin <linziqiao0620@gmail.com>
2017-09-10 15:57:51 -0700

    week2 R-Programming Exercise

--- Untitled.R
+++ Untitled.R
@@ -3,3 +3,13 @@ f <- function(num){
   	cat("Hello World! \n")
   }
 }
+
+f1 <- function(num = 1){ 
+      ## the default value for 'num' is 1
+  hello <- "Hello, World!\n"
+  for (i in seq_len(num)){
+    cat(hello)
+  }
+  chars <- nchar(hello) * num
+  chars
+}
